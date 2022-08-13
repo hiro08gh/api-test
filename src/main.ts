597c5718723a890598d4aaeaeb7e83b9cb411505
hiro08gh <hiro08gh@gmail.com>
2022-08-13 18:07:45 +0900

    add: folder structure

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 const PORT = 3001;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.use(helmet());
+  const config = new DocumentBuilder()
+    .setTitle('nest-api-sample')
+    .setDescription('nest-api-sample')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(PORT);
 }
 
